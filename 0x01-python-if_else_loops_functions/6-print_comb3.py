186bb4302a8114442251f8ce5bbaf75499896035
vdphan <673@holbertonschool.com>
2019-04-30 21:17:23 +0000

    first test for different number

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+for i in range(0, 100):
+    if (i == 1 or ((i % 10) > (i / 10)) and ((i % 10) - (i / 10)) > 1):
+        print("{:0>2d}, ".format(i), end="")
+    if i == 89:
+        print("{:d}".format(i))
