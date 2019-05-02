cbd9a73f6fd491ffbdef82dad8e1f37beefb0c11
vdphan <673@holbertonschool.com>
2019-05-02 19:50:37 +0000

    fix condition

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    print("{} arguments".format(len(argv) - 1), end="")
+    if len(argv) == 2:
+        print("{} argument".format(len(argv) - 1), end="")
+    else:
+        print("{} arguments".format(len(argv) - 1), end="")
     if len(argv) > 1:
         print(":")
     else:
