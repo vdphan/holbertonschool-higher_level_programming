8f051a4f3706efff446491e1fe2dc7b942a4bd52
vdphan <673@holbertonschool.com>
2019-05-02 19:44:37 +0000

    fix typo

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    print("{} arguments".format(len(argv) - 1))
+    print("{} arguments".format(len(argv) - 1), end="")
+    if len(argv) > 1:
+        print(":")
+    else:
+        print(".")
     for i in range(len(argv)):
         if i != 0:
             print("{}: {}".format(i, argv[i]))
