c999fe932bab75bbcd0dd70486df9d6d934155a8
vdphan <673@holbertonschool.com>
2019-05-02 20:37:16 +0000

    print compile file

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4
+    for i in range(len(dir(hidden_4))):
+        if dir(hidden_4)[i][0:2] != "__":
+            print(dir(hidden_4)[i])
