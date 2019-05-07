f2120566a85d34676a4914422e8ca523d7c3fde4
vdphan <673@holbertonschool.com>
2019-05-07 18:15:38 +0000

    replace element

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    if idx > (len(my_list) - 1) or idx < 0:
+        return my_list
+    for i in my_list:
+        if i == idx:
+            my_list[i] = element
+            return my_list
