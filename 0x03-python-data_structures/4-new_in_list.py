0d14a69dc3c81be8f02b78f147591f98fbab60f4
vdphan <673@holbertonschool.com>
2019-05-07 20:00:07 +0000

    replace a copy

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if 0 <= idx < len(my_list):
+        new = my_list[:]
+        new[idx] = element
+        return new
+    return my_list
