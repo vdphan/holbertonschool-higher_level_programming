441ce1f3a21c94bc4c8c57ea35620fa466c773c5
vdphan <673@holbertonschool.com>
2019-05-07 19:37:11 +0000

    fix condition

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    if idx > (len(my_list) - 1) or idx < 0:
+    if 0 <= idx < len(my_list):
+        my_list[idx] = element
         return my_list
-    for i in my_list:
-        if i == idx:
-            my_list[i] = element
-            return my_list
