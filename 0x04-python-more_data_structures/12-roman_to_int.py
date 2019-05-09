10ea920920ea2c9b44fac3bd6ec70444e093aecd
vdphan <673@holbertonschool.com>
2019-05-09 23:30:42 +0000

    check type of string

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,10 +3,10 @@ def roman_to_int(roman_string):
     n = 0
     m = 0
     d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    if roman_string or roman_string != None:
-        for i in range(0, len(roman_string)):
+    if roman_string or type(roman_string) == str:
+        for i in roman_string:
             for j in d:
-                if roman_string[i] == j:
+                if i == j:
                     if d[j] > m:
                         n += d[j] - 2 * m
                     else:
