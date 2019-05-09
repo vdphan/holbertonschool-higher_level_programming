a0493097379424345596c75864d186fcc4fa8501
vdphan <673@holbertonschool.com>
2019-05-09 23:23:07 +0000

    print roman numerical

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    n = 0
+    m = 0
+    d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    if roman_string:
+        for i in range(0, len(roman_string)):
+            for j in d:
+                if roman_string[i] == j:
+                    if d[j] > m:
+                        n += d[j] - 2 * m
+                    else:
+                        n += d[j]
+                    m = d[j]
+        return n
+    return 0
