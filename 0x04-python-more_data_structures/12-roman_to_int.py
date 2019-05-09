7351e1ac193683697e6dca2ebaff7f19090e6721
vdphan <673@holbertonschool.com>
2019-05-09 23:24:53 +0000

    check if string is None

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,7 @@ def roman_to_int(roman_string):
     n = 0
     m = 0
     d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    if roman_string:
+    if roman_string or roman_string != None:
         for i in range(0, len(roman_string)):
             for j in d:
                 if roman_string[i] == j:
