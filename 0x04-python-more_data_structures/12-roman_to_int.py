501019e3010d86ef770423910de99d28bc71835f
vdphan <673@holbertonschool.com>
2019-05-09 23:36:37 +0000

    check type

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,11 +3,11 @@ def roman_to_int(roman_string):
     n = 0
     m = 0
     d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    if roman_string:
+    if roman_string and type(roman_string) == str:
         for i in roman_string:
             for j in d:
                 if i == j:
-                    if d[j] >= m:
+                    if d[j] > m:
                         n += d[j] - 2 * m
                     else:
                         n += d[j]
