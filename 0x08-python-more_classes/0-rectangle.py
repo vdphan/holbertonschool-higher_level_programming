1b10eaacf518f1522e81c9a2b27b0864bdc925c4
vdphan <673@holbertonschool.com>
2019-05-28 17:46:43 +0000

    simple rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module to write an empty class Rectangle that defines a rectangle:
+"""
+
+
+class Rectangle:
+    """This is an empty class Square that defines a rectangle"""
+    pass
