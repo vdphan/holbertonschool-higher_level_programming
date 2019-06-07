ef51bfe9ba61a3644224f8fdbe4ac703302338e4
vdphan <673@holbertonschool.com>
2019-06-07 20:38:12 +0000

    add docuent and condition for key in slots

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
+"""
+This module is to add attribute to object.
+"""
+
+
 def add_attribute(object, key, value):
+    """function that add attribute"""
     if not hasattr(object, "__dict__") and not hasattr(object, "__slots__"):
         raise TypeError("can't add new attribute")
+    if hasattr(object, "__slots__"):
+        if key not in object.__slots__:
+            raise TypeError("can't add new attribute")
     setattr(object, key, value)
