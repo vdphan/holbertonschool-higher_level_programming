6310b0bf9a67301a220a2a69613f3c548d109b07
vdphan <673@holbertonschool.com>
2019-06-11 18:25:39 +0000

    add base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,6 +4,10 @@ This module is to create a class Base.
 """
 
 
+import json
+import os.path
+
+
 class Base:
     """This class is the “base” of all other classes"""
     __nb_objects = 0
@@ -15,3 +19,55 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """returns the JSON string representation of list_dictionaries"""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """writes the JSON string representation of list_objs to a file"""
+        name = cls.__name__ + ".json"
+        with open(name, "w") as f:
+            l = []
+            if list_objs is not None:
+                for obj in list_objs:
+                    l.append(obj.to_dictionary())
+                result = cls.to_json_string(l)
+                return f.write(result)
+            else:
+                return f.write(l)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """returns the list of the JSON string representation json_string:"""
+        if json_string is None or json_string == []:
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """returns an instance with all attributes already set"""
+        #if type(dictionary) is not dict:
+         #   raise
+        value = cls(1, 1)
+        value.update(**dictionary)
+
+        return value
+
+    @classmethod
+    def load_from_file(cls):
+        """function returns a list of instances"""
+        fname = cls.__name__ + ".json"
+        l = []
+        if not os.path.exists(fname):
+            return l
+        with open(fname, "r") as f:
+            output = cls.from_json_string(f.read())
+            print(output)
+            for ele in output:
+                l.append(cls.create(**ele))
+            return l
