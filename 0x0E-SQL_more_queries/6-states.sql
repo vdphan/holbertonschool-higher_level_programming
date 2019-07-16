8a121813b7b6cdc9a5eacadccae6719df50f1e07
vdphan <duyphan0@gmail.com>
2019-07-16 20:06:13 +0000

    add 6

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa)
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- create table states
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+       id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+       name VARCHAR(256) NOT NULL
+);
