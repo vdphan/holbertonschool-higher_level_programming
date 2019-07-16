470da4023e9fc1492ae2c8a0acadab2a1c9c82b0
vdphan <duyphan0@gmail.com>
2019-07-16 20:19:43 +0000

    add 7

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,9 @@
+-- creates the database hbtn_0d_usa and the table cities
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- create cities table
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+       id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+       state_id INT NOT NULL,
+       name VARCHAR(256) NOT NULL,
+       FOREIGN KEY (state_id) REFERENCES states(id)
+);
