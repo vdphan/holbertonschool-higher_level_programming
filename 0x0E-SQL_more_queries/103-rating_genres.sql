6c315325eeecfc30063759929cef319adc07421c
vdphan <duyphan0@gmail.com>
2019-07-17 01:16:01 +0000

    add 103

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,5 +1,5 @@
 -- lists all genres in the database hbtn_0d_tvshows_rate by their rating
-SELECT DISTINCT tv_genres.name, SUM(tv_show_ratings.rate) AS rating
+SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating
 FROM tv_genres
 LEFT JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
 LEFT JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_show_genres.show_id
