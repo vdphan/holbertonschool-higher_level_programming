14981ee4d9dd35b6218f2b7a2d9c88ff06935cdb
vdphan <duyphan0@gmail.com>
2019-08-09 19:54:10 +0000

    add if name = main

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 # lists all states from the database hbtn_0e_0_usa
-import MySQLdb
-
-
-conn = MySQLdb.connect(host="localhost", port=3306,
-                       user="root", passwd="root",
-                       db="hbtn_0e_0_usa", charset="utf8")
-cur = conn.cursor()
-cur.execute("SELECT * FROM states ORDER BY id ASC")
-query_rows = cur.fetchall()
-for row in query_rows:
-    print(row)
-cur.close()
-conn.close()
+if __name__ == "__main__":
+    import MySQLdb
+    conn = MySQLdb.connect(host="localhost", port=3306,
+                           user="root", passwd="root",
+                           db="hbtn_0e_0_usa", charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 # lists all states with a name starting with N (upper N)
-import MySQLdb
-
-
-conn = MySQLdb.connect(host="localhost", port=3306,
-                       user="root", passwd="root",
-                       db="hbtn_0e_0_usa", charset="utf8")
-cur = conn.cursor()
-cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-query_rows = cur.fetchall()
-for row in query_rows:
-    print(row)
-cur.close()
-conn.close()
+if __name__ == "__main__":
+    import MySQLdb
+    conn = MySQLdb.connect(host="localhost", port=3306,
+                           user="root", passwd="root",
+                           db="hbtn_0e_0_usa", charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
