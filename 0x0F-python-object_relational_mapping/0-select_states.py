332e041f496fc4be8e6bb8fbadb0938a7362948a
vdphan <duyphan0@gmail.com>
2019-08-09 19:17:24 +0000

    fix pep8 error

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-#lists all states from the database hbtn_0e_0_usa
+# lists all states from the database hbtn_0e_0_usa
 import MySQLdb
 
 
-conn = MySQLdb.connect(host="localhost", port=3306, user="root", passwd="root", db="hbtn_0e_0_usa", charset="utf8")
+conn = MySQLdb.connect(host="localhost", port=3306,
+                       user="root", passwd="root",
+                       db="hbtn_0e_0_usa", charset="utf8")
 cur = conn.cursor()
 cur.execute("SELECT * FROM states ORDER BY id ASC")
 query_rows = cur.fetchall()
