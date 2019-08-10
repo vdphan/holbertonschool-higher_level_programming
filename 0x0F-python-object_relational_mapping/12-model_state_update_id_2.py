79b424f6ceb345dc713e271133a9e81729d12913
vdphan <duyphan0@gmail.com>
2019-08-10 21:23:26 +0000

    remove add

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -13,8 +13,6 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    new = State(name="Louisiana")
-    session.add(new)
     session.query(State).filter_by(id=2).update({"name": ("New Mexico")})
     session.commit()
     session.close()
