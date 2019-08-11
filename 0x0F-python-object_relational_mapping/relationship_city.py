5efe821360b3994c5a9ee8341ac9ef3139f0a05d
vdphan <duyphan0@gmail.com>
2019-08-11 08:03:52 +0000

    fix import error

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-""" a python file that contains the class definition of a State
-and an instance Base = declarative_base()
-"""
+# a python file that contains the class definition of a State
+# and an instance Base = declarative_base()
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, ForeignKey
-from model_state import Base, State
 from sqlalchemy import ForeignKey
+from relationship_state import State, Base
 
 
 class City(Base):
