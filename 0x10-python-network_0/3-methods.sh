b2d113d219fc9d417d19a8461aee468c5bb4fc61
vdphan <duyphan0@gmail.com>
2019-08-20 20:58:30 +0000

    fix cut

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a Bash script that takes in a URL and displays all HTTP methods the server will accept
-curl -s -i "$1" | grep -i Allow: | cut -d " " -f 2,3,4
+curl -s -i "$1" | grep -i Allow: | cut -d " " -f 2-
