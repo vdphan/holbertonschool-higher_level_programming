5a45321807bb3ded18115668f94dc0a1de69234c
vdphan <duyphan0@gmail.com>
2019-08-26 19:27:15 +0000

    change to tab

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,6 +6,6 @@ import urllib.request
 with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
     a = response.read()
     print("Body response:")
-    print("    - type: {}".format(type(a)))
-    print("    - content: {}".format(a))
-    print("    - utf8 content: {}".format(a.decode("utf-8")))
+    print("\t- type: {}".format(type(a)))
+    print("\t- content: {}".format(a))
+    print("\t- utf8 content: {}".format(a.decode("utf-8")))
