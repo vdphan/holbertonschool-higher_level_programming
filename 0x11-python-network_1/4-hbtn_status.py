38a7da4400658a66f12852f930452c0b045513d3
vdphan <duyphan0@gmail.com>
2019-08-26 21:09:29 +0000

    add 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 # a Python script that fetches https://intranet.hbtn.io/status
-import urllib.request
+import requests
 
-
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-    a = response.read()
+if __name__ == "__main__":
+    r = requests.get('https://intranet.hbtn.io/status')
     print("Body response:")
-    print("\t- type: {}".format(type(a.decode("utf-8"))))
-    print("\t- content: {}".format(a))
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
