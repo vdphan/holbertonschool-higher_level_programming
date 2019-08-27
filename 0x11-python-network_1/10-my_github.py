3b5b3ca6061d53f2e3cf6b1bfc7202156cae0817
vdphan <duyphan0@gmail.com>
2019-08-27 00:42:30 +0000

    add 10

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""a Python script that takes your Github credentials
+(username and password) and uses the Github API to display your id
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    r = requests.get('https://api.github.com/user', auth=(sys.argv[1],
+                                                          sys.argv[2]))
+    try:
+        print(r.json()['id'])
+    except:
+        print("None")
