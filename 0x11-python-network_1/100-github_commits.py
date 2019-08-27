3851431364f8bcc39b4baa6df73f2c83ce823b77
vdphan <duyphan0@gmail.com>
2019-08-27 01:43:09 +0000

    fix argv order

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -8,7 +8,7 @@ import sys
 
 if __name__ == "__main__":
     r = requests.get('https://api.github.com/repos/{}/{}/commits'.
-                     format(sys.argv[1], sys.argv[2]))
+                     format(sys.argv[2], sys.argv[1]))
     if len(r.json()) >= 10:
         for i in range(0, 10):
             print("{}: {}".format(r.json()[i].get('sha'),
