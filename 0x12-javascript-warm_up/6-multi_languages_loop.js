c680634f1ca4b531a26a61226287abd2394f69b8
vdphan <duyphan0@gmail.com>
2019-09-17 19:45:07 +0000

    change for loop

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -4,4 +4,6 @@ a script that prints 3 lines: (like 1-multi_languages.js)
 but by using an array of string and a loop
 */
 const array = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-array.forEach(item => console.log(item));
+for (const item of array) {
+  console.log(item);
+}
