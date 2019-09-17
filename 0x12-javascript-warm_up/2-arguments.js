eaa141dabcb9aa0977bb459cf5eb8e53c69b628e
vdphan <duyphan0@gmail.com>
2019-09-17 19:41:10 +0000

    add more condition for multiple argvs

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,8 +2,10 @@
 /*
 a script that prints a message depending of the number of arguments passed
 */
-if (process.argv.length >= 3) {
+if (process.argv.length === 3) {
   console.log('Argument found');
+} else if (process.argv.length > 3) {
+  console.log('Arguments found')
 } else {
   console.log('No argument');
 }
