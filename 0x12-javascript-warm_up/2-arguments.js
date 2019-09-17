b68ba96d0c680028342da1a0858d2facd68d1b2a
vdphan <duyphan0@gmail.com>
2019-09-17 19:42:15 +0000

    fix semistandard check

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -5,7 +5,7 @@ a script that prints a message depending of the number of arguments passed
 if (process.argv.length === 3) {
   console.log('Argument found');
 } else if (process.argv.length > 3) {
-  console.log('Arguments found')
+  console.log('Arguments found');
 } else {
   console.log('No argument');
 }
