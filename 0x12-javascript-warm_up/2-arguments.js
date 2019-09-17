aa21e0a7412d0543e9bfc52806681583e2463d7a
vdphan <duyphan0@gmail.com>
2019-09-17 17:47:04 +0000

    add task 2

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/*
+a script that prints a message depending of the number of arguments passed
+*/
+if (process.argv.length >= 3) {
+  console.log('Argument found');
+} else {
+  console.log('No argument');
+}
