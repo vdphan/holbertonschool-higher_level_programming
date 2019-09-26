b9e092d61a40e6f31bf9aa3627cd9d5bac1bab29
vu phan <vuphan@vus-MacBook-Pro.local>
2019-09-26 13:36:48 -0700

    Add task 8

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,1 +1,5 @@
-#!/usr/bin/node
+$.get('https://swapi.co/api/films/?format=json', function(data){
+    for (let movie of data.results) {
+        $('UL#list_movies').append(`<li>${movie.title}</li>`);
+    }
+});
