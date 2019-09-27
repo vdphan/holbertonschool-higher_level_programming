ffb68aec2aa86cc19efe7b134fc25973de5472be
vu phan <vuphan@vus-macbook-pro.attlocal.net>
2019-09-26 21:57:09 -0700

    add task 13

--- 0x15-javascript-web_jquery/103-script.js
+++ 0x15-javascript-web_jquery/103-script.js
@@ -1,1 +1,15 @@
+$( document ).ready(function() {
+    
+    $('INPUT#btn_translate').click(function() {
+        const code = $('#language_code').val()
+        $.getJSON('https://fourtonfish.com/hellosalut/?lang=' + code, function(data){
+             $('DIV#hello').html(data.hello);
+        });
+    });
 
+    $('INPUT#language_code').on('keypress', function (e) {
+        if(e.which == 13){
+            $('INPUT#btn_translate').click();
+        }
+    });
+});
